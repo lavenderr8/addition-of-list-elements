dcbc25e7696faa2802695f80661722129c476387
lavander8 <lavander8@rambler.su>
2025-10-17 17:56:27 +0300

    Исправлен код

--- list_addition_program.py
+++ list_addition_program.py
@@ -1,10 +1,21 @@
 list_1 = [0, 2, 2, 4, 4]
 list_2 = [9, 6, 7, 4, 5]
+
+
+def get_element_or_0(lst, index):
+    if index >= len(lst):
+        return 0
+    return lst[index]
+
+
 list_3 = []
 n = len(list_1)
+l = len(list_2)
+m = max(n, l)
 
-for i in range(n):
-    item = list_1[i] + list_2[i]
+for i in range(m):
+    item = get_element_or_0(list_1, i) + get_element_or_0(list_2, i)
     list_3.append(item)
 
 print(list_3)
+print(m)
